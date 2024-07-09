c340d4b87372bf1635610c0d87fe45ed5962e706
rCotche <roshan.cotche@gmail.com>
2024-07-09 13:58:18 +0400

    structure done

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dashboard_ui_example/widgets/side_menu_widget.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
@@ -5,6 +6,40 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              // 2/12
+              //*100
+              //16%
+              flex: 2,
+              child: SizedBox(
+                child: SideMenuWidget(),
+              ),
+            ),
+            Expanded(
+              // 7/12
+              //*100
+              //58%
+              flex: 7,
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+            Expanded(
+              // 3/12
+              //*100
+              //25%
+              flex: 3,
+              child: Container(
+                color: Colors.green,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/side_menu_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SideMenuWidget extends StatefulWidget {
+  const SideMenuWidget({super.key});
+
+  @override
+  State<SideMenuWidget> createState() => _SideMenuWidgetState();
+}
+
+class _SideMenuWidgetState extends State<SideMenuWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
